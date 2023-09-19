4b0774e90507a3db3c8339d22de84502548c12ff
lecopeaux <lenny.copeaux@etu.u-bordeaux.fr>
2023-09-19 15:05:57 +0200

    Refactoring des tests

--- php/tests/MoneyProblem/ConvertMoneyTest.php
+++ php/tests/MoneyProblem/ConvertMoneyTest.php
@@ -9,34 +9,35 @@ use PHPUnit\Framework\TestCase;
 
 class ConvertMoneyTest extends TestCase
 {
-
+    private $bank;
+    protected function setUp(): void {
+        parent::setUp();
+        $this->bank =  Bank::create(Currency::EUR(), Currency::USD(), 1.2);;
+    }
+    
     public function test_bank_convert_eur_to_usd()
     {
-        $this->assertEquals(12, Bank::create(Currency::EUR(), Currency::USD(), 1.2)->convert(10, Currency::EUR(), Currency::USD()));
+        $converted_money = $this->bank->convert(10, Currency::EUR(), Currency::USD());
+        $this->assertEquals(12, $converted_money);
     }
 
     public function test_bank_convert_eur_to_eur()
     {
-        $this->assertEquals(10, Bank::create(Currency::EUR(), Currency::USD(), 1.2)->convert(10, Currency::EUR(), Currency::EUR()));
+        $converted_money = $this->bank->convert(10, Currency::EUR(), Currency::EUR());
+        $this->assertEquals(10, $converted_money);
     }
 
-    public function test_bank_convert_throws_exception_on_missing_exchange_rate()
+    public function test_bank_convert_without_exchange_rate()
     {
         $this->expectException(MissingExchangeRateException::class);
         $this->expectExceptionMessage('EUR->KRW');
-
-        Bank::create(Currency::EUR(), Currency::USD(), 1.2)->convert(10, Currency::EUR(), Currency::KRW());
+        $this->bank->convert(10, Currency::EUR(), Currency::KRW());
     }
 
-    public function test_convert_with_different_exchange_rates_returns_different_floats()
+    public function test_bank_convert_with_different_exchange_rates()
     {
-        $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
-
-        $this->assertEquals(12, $bank->convert(10, Currency::EUR(), Currency::USD()));
-
-        $bank->addEchangeRate(Currency::EUR(), Currency::USD(), 1.3);
-
-        $this->assertEquals(13, $bank->convert(10, Currency::EUR(), Currency::USD()));
+        $this->assertEquals(12, $this->bank->convert(10, Currency::EUR(), Currency::USD()));
+        $this->bank->addEchangeRate(Currency::EUR(), Currency::USD(), 1.3);
+        $this->assertEquals(13, $this->bank->convert(10, Currency::EUR(), Currency::USD()));
     }
-
 }
---
+++ php/tests/MoneyProblem/MoneyCalculatorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\MoneyProblem\Domain;
+
+use MoneyProblem\Domain\Currency;
+use MoneyProblem\Domain\MoneyCalculator;
+use PHPUnit\Framework\TestCase;
+
+class MoneyCalculatorTest extends TestCase
+{
+    public function test_add_in_usd_returns_value()
+    {
+        $money = MoneyCalculator::add(5, Currency::USD(), 10);
+        $this->assertIsFloat($money);
+        $this->assertNotNull($money);
+    }
+
+    public function test_multiply_in_euros_returns_positive_number()
+    {
+        $money = MoneyCalculator::times(10, Currency::USD(), 2);
+        $this->assertLessThan($money, 0);
+    }
+
+    public function test_divide_in_korean_won_returns_float()
+    {
+        $money = MoneyCalculator::divide(4002, Currency::USD(), 4);
+        $this->assertEquals($money, 1000.5);
+    }
+}
