7fb6c71dbf6a87a1ef0c5efc24d559fb5dbf3516
Matis <matis.bouquin@etu.u-bordeaux.fr>
2023-10-17 15:05:13 +0200

    adding test for us

--- php/tests/MoneyProblem/PortfolioTest.php
+++ php/tests/MoneyProblem/PortfolioTest.php
@@ -34,6 +34,10 @@ class Portfolio
         }
         $this->amount[(string) $currency]["amount"] += $amount_to_add;
     }
+
+    public function getAmount(){
+        return $this->amount;
+    }
 }
 
 class PortfolioTest extends TestCase
@@ -46,6 +50,15 @@ class PortfolioTest extends TestCase
         $portfolio = new Portfolio($bank);
         $amout = $portfolio->evaluate(Currency::EUR(), $bank);
         $this->assertEquals(0, $amout);
+    }
+
+    public function test_add_differency_money(){
+        $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
+        $bank = Bank::create(Currency::EUR(), Currency::KRW(), 1.5);
+        $portfolio = new Portfolio($bank);
+        $portfolio->add(10, Currency::USD());
+        $portfolio->add(10, Currency::EUR());
+        $this->assertEquals(2,count($portfolio->getAmount()));
     }
 
     public function test_portfolio_is_evaluated_to_the_same_currency()
@@ -64,5 +77,18 @@ class PortfolioTest extends TestCase
         $portfolio->add(10, Currency::USD());
         $amout = $portfolio->evaluate(Currency::EUR(), $bank);
         $this->assertEquals(12, $amout);
+    }
+
+    public function test_portfolio_add_usd_and_krw_and_eur_evaluate_to_eur(){
+        $bank = Bank::create(Currency::USD(), Currency::EUR(), 1.2);
+        $bank->addEchangeRate(Currency::KRW(), Currency::EUR(), 1.5);
+        $bank->addEchangeRate(Currency::EUR(), Currency::EUR(), 1);
+        $portfolio = new Portfolio($bank);
+        $portfolio->add(10, Currency::USD());
+        $portfolio->add(10, Currency::KRW());
+        $portfolio->add(10, Currency::EUR());
+
+        $amout = $portfolio->evaluate(Currency::EUR());
+        $this->assertEquals(37, $amout);
     }
 }
